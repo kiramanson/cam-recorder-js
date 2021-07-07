f32c1812b1479baa42fbfe1af9c46fcde2dea291
Thiago Moura <cbjrcs@hotmail.com>
2021-07-07 16:35:39 -0300

    constraints adjust

--- cam-recorder-top.js
+++ cam-recorder-top.js
@@ -4,12 +4,10 @@
   const flashButton = document.querySelector("svg.flash");
   const gumVideo = document.querySelector("video#gum");
   let faceCam = true;
-  
+
   function getConstraints() {
     return {
-      audio: {
-        echoCancellation: { exact: true },
-      },
+      audio: false,
       video: {
         /* height: 1920, width: 1080, frameRate: 60, */
         // height: 1080, width: 1920, frameRate: 60,
@@ -19,34 +17,50 @@
       },
     };
   }
+  function getVideoTrack() {
+    let trackReturn;
+    gumVideo.srcObject.getTracks().forEach(function (track) {
+      if (track.kind === "video") trackReturn = track;
+    });
+    return trackReturn;
+  }
 
   startButton.addEventListener("click", async () => {
-    // const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
     /* console.log('Using media constraints:', constraints) */
-	let consT = await getConstraints()
+    let consT = await getConstraints();
     await init(consT);
 
     // createOverlay()
   });
 
   camRotateButton.addEventListener("click", async () => {
-    faceCam = !faceCam;
-    restartCamera();
+    // faceCam = !faceCam;
+    // restartCamera();
+    const track = getVideoTrack();
+
+    let facingMode = track.getConstraints().facingMode;
+
+    track.applyConstraints({
+      facingMode: facingMode === "environment" ? "user" : "environment",
+    });
   });
-  
+
   flashButton.addEventListener("click", async () => {
-    gumVideo
-      .srcObject.getTracks()
-      .forEach(function (track) {
-        track.applyConstraints({
-			advanced: [{torch: true}]
-		  });
-      });
+    const track = await getVideoTrack();
+    console.log(track);
+    let consT = track.getConstraints();
+    let torch = false;
+
+    if (consT.advanced) torch = consT.advanced[0].torch;
+
+    track.applyConstraints({
+      advanced: [{ torch: !torch }],
+    });
   });
 
   function handleSuccess(stream) {
-    /* console.log('getUserMedia() got stream:', stream) */ window.stream =
-      stream;
+    /* console.log('getUserMedia() got stream:', stream) */
+    window.stream = stream;
 
     gumVideo.srcObject = stream;
   }
@@ -62,12 +76,10 @@
   }
 
   async function restartCamera() {
-    gumVideo
-      .srcObject.getTracks()
-      .forEach(function (track) {
-        track.stop();
-      });
-	let consT = await getConstraints()
+    gumVideo.srcObject.getTracks().forEach(function (track) {
+      track.stop();
+    });
+    let consT = await getConstraints();
     await init(consT);
   }
 })();
