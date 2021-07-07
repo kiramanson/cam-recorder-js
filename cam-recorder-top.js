1452cefc6c49a90099c538a80d5d3db34985002d
Thiago Moura <cbjrcs@hotmail.com>
2021-07-07 10:25:12 -0300

    make reactive constraints by function

--- cam-recorder-top.js
+++ cam-recorder-top.js
@@ -3,23 +3,26 @@
   const camRotateButton = document.querySelector("svg.rotate");
   let faceCam = true;
 
-  const constraints = {
-    audio: {
-      echoCancellation: { exact: true },
-    },
-    video: {
-      /* height: 1920, width: 1080, frameRate: 60, */
-      // height: 1080, width: 1920, frameRate: 60,
-      facingMode: faceCam ? "user" : "environment",
-      // facingMode: { exact: "user" },
-      // facingMode: 'facemode',
-    },
-  };
+  function getConstraints() {
+    return {
+      audio: {
+        echoCancellation: { exact: true },
+      },
+      video: {
+        /* height: 1920, width: 1080, frameRate: 60, */
+        // height: 1080, width: 1920, frameRate: 60,
+        facingMode: faceCam ? "user" : "environment",
+        // facingMode: { exact: "user" },
+        // facingMode: 'facemode',
+      },
+    };
+  }
 
   startButton.addEventListener("click", async () => {
     // const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
     /* console.log('Using media constraints:', constraints) */
-    await init(constraints);
+	let consT = await getConstraints()
+    await init(consT);
 
     // createOverlay()
   });
@@ -54,6 +57,7 @@
       .forEach(function (track) {
         track.stop();
       });
-    await init(constraints);
+	let consT = await getConstraints()
+    await init(consT);
   }
 })();
