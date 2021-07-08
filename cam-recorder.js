8d77cb4a8e1e64606db26643dfe33113e6273c6c
Thiago Moura <cbjrcs@hotmail.com>
2021-07-08 16:04:17 -0300

    test: flash debug

--- cam-recorder.js
+++ cam-recorder.js
@@ -7,7 +7,7 @@ class CamRecorder {
     this.flashButton = document.querySelector("svg#flash");
     this.gumVideo = document.querySelector("video#gum");
     this.footer = document.querySelector("p#footer > span")
-    this.capabilities = document.querySelector("p#capabilities > span")
+    this.capabilities = document.querySelector("p#capabilities > code")
     this.faceCam = true;
     this.torch = false
 
@@ -71,7 +71,7 @@ class CamRecorder {
       });
       
       let constraints = track.getConstraints()
-      this.footer.innerHTML = constraints.advanced[0].torch ? 'Ligado' : 'Desligado'
+      this.footer.innerHTML = constraints.torch ? 'Ligado' : 'Desligado'
       
   });
   }
